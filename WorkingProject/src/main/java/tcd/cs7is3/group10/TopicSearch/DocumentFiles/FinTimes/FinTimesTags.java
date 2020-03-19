b0e3cc2cf9efdd76349d0df29fbdb8dfaef96303
Jayprakash Asolkar <jayasolkar@gmail.com>
2020-03-19 11:59:27 +0000

    Working Project

---
+++ WorkingProject/src/main/java/tcd/cs7is3/group10/TopicSearch/DocumentFiles/FinTimes/FinTimesTags.java
@@ -0,0 +1,18 @@
+package tcd.cs7is3.group10.TopicSearch.DocumentFiles.FinTimes;
+
+public enum FinTimesTags {
+
+    TEXT_START("<TEXT>"), TEXT_END("</TEXT>"), HEADLINE_START("<HEADLINE>"), HEADLINE_END("</HEADLINE>"), BYLINE_START(
+            "<BYLINE>"), BYLINE_END("</BYLINE>"), DOC_NO_START("<DOCNO>"), DOC_NO_END("</DOCNO>"), DOC_ID_START(
+            "<DOCID>"), DOC_ID_END("</DOCID>"), DOC_START("<DOC>"), DOC_END("</DOC>"),DATE_START("<DATE>"),DATE_END("</DATE>");
+
+    String tag;
+
+    FinTimesTags(final String tag) {
+        this.tag = tag;
+    }
+
+    public String getTag() {
+        return this.tag;
+    }
+}

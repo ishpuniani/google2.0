af61c9b8aa5208df449a5cc9b062706a6c91ccfb
Andrew D <24252685+Donegaan@users.noreply.github.com>
2020-04-03 16:00:12 +0100

    Increasing java heap size

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 # Commands to run program and store results
 
-java -cp target/app-1.0-SNAPSHOT.jar com.mycompany.app.SearchIndex
+java -cp -Xmx6g target/app-1.0-SNAPSHOT.jar com.mycompany.app.SearchIndex
 
 ./trec_eval-9.0.7/trec_eval DataSet/qrels.assignment2.part1 results.txt > trec_results.txt
 
